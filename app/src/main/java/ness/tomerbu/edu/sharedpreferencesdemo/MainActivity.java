b424de1dee889bc4b4b69718d84ee0254b2cc8dc
nesscollege2016 <nesscollege2016@gmail.com>
2016-06-13 10:34:15 +0300

    Initial commit

---
+++ app/src/main/java/ness/tomerbu/edu/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,87 @@
+package ness.tomerbu.edu.sharedpreferencesdemo;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements TextWatcher {
+    private EditText etNote;
+    private SharedPreferences prefs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        etNote = (EditText) findViewById(R.id.etNote);
+        prefs = getSharedPreferences("Notes", MODE_PRIVATE);
+
+
+        String note = loadNote();
+        etNote.setText(note);
+
+       etNote.addTextChangedListener(this);
+
+    }
+
+    private void saveNote(String text){
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("note", text);
+
+        editor.apply();
+
+    }
+
+    private String loadNote(){
+        String note = prefs.getString("note", null);
+        if (note!=null){
+            return note;
+        }
+        return "";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        saveNote(editable.toString());
+    }
+}
